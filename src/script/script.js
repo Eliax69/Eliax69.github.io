c57cbee264247b3f3dd7cd9c68cce0fc1bc2edbe
Eliax <eliaxem69@gmail.com>
2022-07-08 01:23:55 -0300

    Correção responsividade
    Imagem do celular na section phone-section, estava com min-width, resultando em um bug.

--- src/script/script.js
+++ src/script/script.js
@@ -1,81 +1,81 @@
-ScrollReveal({ reset: true });
-window.sr = new ScrollReveal();
+// ScrollReveal({ reset: true });
+// window.sr = new ScrollReveal();
 
-var left = [
-    ".logo",
-    ".box-title",
-    ".box-list",
-    ".box-button",
-    ".left-content",
-    ".form-title",
-    ".form-div",
-    ".left-footer"
-]
+// var left = [
+//     ".logo",
+//     ".box-title",
+//     ".box-list",
+//     ".box-button",
+//     ".left-content",
+//     ".form-title",
+//     ".form-div",
+//     ".left-footer"
+// ]
 
-var right = [
-    ".phone",
-    ".menu",
-    ".right-content",
-    ".contact-right",
-    ".right-footer",
-    ".responsive-menu"
-]
+// var right = [
+//     ".phone",
+//     ".menu",
+//     ".right-content",
+//     ".contact-right",
+//     ".right-footer",
+//     ".responsive-menu"
+// ]
 
-var bottom = [
-    ".text-box",
-    ".box-content",
-    ".center-image"
-]
+// var bottom = [
+//     ".text-box",
+//     ".box-content",
+//     ".center-image"
+// ]
 
-class hidden {
-    constructor(classToAdd, direction){
-        this.classToAdd = classToAdd;
-        this.direction = direction;
-    }
+// class hidden {
+//     constructor(classToAdd, direction){
+//         this.classToAdd = classToAdd;
+//         this.direction = direction;
+//     }
 
-    init(){
-        this.classToAdd ? this.effect() : console.log('An error has occurred with your hidden class.')
-    }
+//     init(){
+//         this.classToAdd ? this.effect() : console.log('An error has occurred with your hidden class.')
+//     }
 
-    effect(){
-        this.direction == "left" ? (
-            this.classToAdd.forEach(myClass => {
-                sr.reveal(myClass, {
-                    delay: 300,
-                    origin: 'left',
-                    distance: '55px'
-                })
-            })
-        ) : null
+//     effect(){
+//         this.direction == "left" ? (
+//             this.classToAdd.forEach(myClass => {
+//                 sr.reveal(myClass, {
+//                     delay: 300,
+//                     origin: 'left',
+//                     distance: '55px'
+//                 })
+//             })
+//         ) : null
 
-        this.direction == "right" ? (
-            this.classToAdd.forEach((myClass) => {
-                sr.reveal(myClass, {
-                    delay: 300,
-                    origin: 'right',
-                    distance: '55px'
-                })
-            })
-        ) : null
+//         this.direction == "right" ? (
+//             this.classToAdd.forEach((myClass) => {
+//                 sr.reveal(myClass, {
+//                     delay: 300,
+//                     origin: 'right',
+//                     distance: '55px'
+//                 })
+//             })
+//         ) : null
         
-        this.direction == "bottom" ? (
-            this.classToAdd.forEach((myClass) => {
-                sr.reveal(myClass, {
-                    delay: 300,
-                    origin: 'bottom',
-                    distance: '55px'
-                })
-            })
-        ) : null
-    }
-}
+//         this.direction == "bottom" ? (
+//             this.classToAdd.forEach((myClass) => {
+//                 sr.reveal(myClass, {
+//                     delay: 300,
+//                     origin: 'bottom',
+//                     distance: '55px'
+//                 })
+//             })
+//         ) : null
+//     }
+// }
 
-var leftElements = new hidden(left, "left")
-leftElements.init()
-var rightElements = new hidden(right, "right")
-rightElements.init()
-var bottomElements = new hidden(bottom, "bottom")
-bottomElements.init()
+// var leftElements = new hidden(left, "left")
+// leftElements.init()
+// var rightElements = new hidden(right, "right")
+// rightElements.init()
+// var bottomElements = new hidden(bottom, "bottom")
+// bottomElements.init()
 
 class Menu {
     constructor(links){
@@ -169,8 +169,8 @@ class Phone {
 
     effect()
     {
-        var dark = "images/cellphone.png";
-        var white = "images/cellphone_white.png";
+        var dark = "src/images/cellphone.png";
+        var white = "src/images/cellphone_white.png";
 
         this.power.addEventListener("click", () => {
             this.phone.classList.contains("dark") ? (
